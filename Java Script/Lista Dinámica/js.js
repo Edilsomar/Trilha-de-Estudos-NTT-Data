5b474a568d7bd24113f4578cb69a359a896728e5
Edilsomar Oliveira <oliveiraedilsomar@gmail.com>
2026-03-13 13:10:30 -0300

    Add files via upload

---
+++ Java Script/Lista Dinámica/js.js
@@ -0,0 +1,10 @@
+const func = ['Alan Rodrígues', 'Barbara Macedo', 'Claudio Rocha', 'Daniela Monteiro', 'Eduarda Kelly'];
+
+var lista = document.getElementById('lista');
+
+for(let pos = 0 ; pos < func.length; pos++){
+    var funci = func[pos];
+    var li = document.createElement('li');
+    li.innerText = funci;
+    lista.appendChild(li);
+}
