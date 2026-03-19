10924cf4d33b190bef8cdf8469ee47dafc6358ff
Edilsomar Oliveira <oliveiraedilsomar@gmail.com>
2026-03-19 16:42:17 -0300

    Add files via upload

---
+++ React/props/PropsTypes & DefaultProps/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import List from './components/List';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Olá!</h1>
+       <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/props/PropsTypes & DefaultProps/List.js
@@ -0,0 +1,15 @@
+import Item from './Item';
+function List(){
+    return(
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <Item marca="Ferrari" ano_lancamento={1985} />
+                <Item marca="Renault" ano_lancamento={1898} />
+                <Item />
+            </ul>
+        </>
+    )
+
+}
+export default List;
