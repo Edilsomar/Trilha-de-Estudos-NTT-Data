3288ea5ba8c76c366f657222b14e8d4ffe81b7aa
Edilsomar Oliveira <oliveiraedilsomar@gmail.com>
2026-03-21 09:44:05 -0300

    Add files via upload

---
+++ React/events/Form.js
@@ -0,0 +1,21 @@
+function Form(){
+
+    function cadastrarUsuario(event){
+        event.preventDefault() //bloqueia o comportamento padrão do html (que seria enviar o form pro servidor)
+        console.log('Usuário cadastrado!') //e mostra isso no console.
+    }
+    return (
+        <div>
+            <h1>Meu Cadastro:</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <input type="text" placeholder="Digite o seu nome" />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Form;
