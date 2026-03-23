cf0d2b3504a2dc4800cf7c1b7388106b75b93a89
Edilsomar Oliveira <oliveiraedilsomar@gmail.com>
2026-03-23 13:10:55 -0300

    Update SayMyName.js

--- React/props/SayMyName.js
+++ React/props/SayMyName.js
@@ -9,3 +9,11 @@ function SayMyName(props){
 export default SayMyName;
 
 // Aprendi a criar argumentos e chama-los na página principal!
+
+// props = objeto com dados do componente
+
+// (props) = recebe esses dados
+
+// props.nome = acessa o valor
+
+// { nome } = desestruturação (forma melhor)
